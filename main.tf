605f2ea292a8c722250a3d721026ff9bed4ce6ec
gcastill0 <gilberto@hashicorp.com>
2021-02-11 16:04:30 -0500

    Removed tenant

--- main.tf
+++ main.tf
@@ -6,23 +6,23 @@ terraform {
   }
 }
 
-/* Configure the provider with your Cisco APIC credentials for 
-- APIC Username
--  APIC Password
-- APIC URL
-*/
-provider "aci" {
-  username = var.username
-  password = var.password
-  url      = var.apic_url
-  insecure = true
-}
+#  Configure the provider with your Cisco APIC credentials for 
+# - APIC Username
+# -  APIC Password
+# - APIC URL
 
-# Define an ACI Tenant Resource. 
-resource "aci_tenant" "terraform_tenant" {
-  name        = var.tenant
-  description = var.description
-}
+# provider "aci" {
+#   username = var.username
+#   password = var.password
+#   url      = var.apic_url
+#   insecure = true
+# }
+
+# # Define an ACI Tenant Resource. 
+# resource "aci_tenant" "terraform_tenant" {
+#   name        = var.tenant
+#   description = var.description
+# }
 
 # # 2 
 
