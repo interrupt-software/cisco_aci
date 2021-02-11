e1edd9eb957284aeda5ce256ee933bb0581d7f72
gcastill0 <gilberto@hashicorp.com>
2021-02-11 12:02:02 -0500

    Adding Tenant Policy/Contract Resources

--- intro_module/variables.tf
+++ intro_module/variables.tf
@@ -12,6 +12,7 @@ variable "description" {
   default = "Workshop Demo on Thu Feb 11, 2021"
 }
 
+# 2
 variable "vrf" {
   type    = string
   default = "prod_vrf"
@@ -23,4 +24,41 @@ variable "bd" {
 variable "subnet" {
   type    = string
   default = "10.10.101.1/24"
+}
+
+# 3
+
+able "filters" {
+  description = "Create filters with these names and ports"
+  type        = map(any)
+  default = {
+    filter_https = {
+      filter   = "https",
+      entry    = "https",
+      protocol = "tcp",
+      port     = "443"
+    },
+    filter_sql = {
+      filter   = "sql",
+      entry    = "sql",
+      protocol = "tcp",
+      port     = "1433"
+    }
+  }
+}
+variable "contracts" {
+  description = "Create contracts with these filters"
+  type        = map(any)
+  default = {
+    contract_web = {
+      contract = "web",
+      subject  = "https",
+      filter   = "filter_https"
+    },
+    contract_sql = {
+      contract = "sql",
+      subject  = "sql",
+      filter   = "filter_sql"
+    }
+  }
 }
