3d4ffa4bc1d0e0b623273611c570071eb66e3552
gcastill0 <gilberto@hashicorp.com>
2021-03-16 11:31:18 -0400

    Making changes for workshop Mar 16

--- main.tf
+++ main.tf
@@ -6,23 +6,23 @@ terraform {
   }
 }
 
-#  Configure the provider with your Cisco APIC credentials for 
-# - APIC Username
-# -  APIC Password
-# - APIC URL
+ Configure the provider with your Cisco APIC credentials for 
+- APIC Username
+-  APIC Password
+- APIC URL
 
-# provider "aci" {
-#   username = var.username
-#   password = var.password
-#   url      = var.apic_url
-#   insecure = true
-# }
+provider "aci" {
+  username = var.username
+  password = var.password
+  url      = var.apic_url
+  insecure = true
+}
 
-# # Define an ACI Tenant Resource. 
-# resource "aci_tenant" "terraform_tenant" {
-#   name        = var.tenant
-#   description = var.description
-# }
+# Define an ACI Tenant Resource. 
+resource "aci_tenant" "terraform_tenant" {
+  name        = var.tenant
+  description = var.description
+}
 
 # # 2 
 
